1a367bccf23318df66988be7086ebd10554f7191
C-github123 <3138836384@qq.com>
2024-08-20 21:30:56 +0800

    更新 try.cpp

--- try.cpp
+++ try.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main(){
     cout<<"hello world!"<<endl;
     cout << "hello world! by smatur" << endl;
+    cout << "hello world! by C" << endl;
     cout << "test" << endl;
     return 0;
 }
